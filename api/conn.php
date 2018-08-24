00aeee7c75caa075a4b05404123a6612a22ef21c
782483506 <782483506@qq.com>
2018-08-24 11:49:15 +0800

    update password

--- api/conn.php
+++ api/conn.php
@@ -1,5 +1,5 @@
 <?php
-$conn=mysql_connect("localhost","root","20151010215");
+$conn=mysql_connect("localhost","root","");
 mysql_select_db("angular_book",$conn);
 mysql_query("set names utf8");
 ?>
